0bef7c9f71dc4ff406370103c6baf008434d50cb
LizethCs <lcangulo26@gmail.com>
2024-10-27 16:58:43 -0500

    add Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return(
+  <>
+    <footer className="flex">
+      <p className="contact" id="contacto">
+        Contactanos:
+      </p>
+      <p></p>
+      <p className="contact">email.adress@contact.com</p>
+      <p className="contact">+61 000 003 008</p>
+    </footer>
+  </>)
+}
